f95861011bc0b9160a81e19963fe39ba42dbe2c1
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2022-06-13 09:22:39 -0700

    feat: remove remix blues template app

--- app/routes/logout.tsx
+++
@@ -1,8 +0,0 @@
-import type { ActionFunction, LoaderFunction } from '@remix-run/node';
-import { redirect } from '@remix-run/node';
-
-import { logout } from '~/session.server';
-
-export const action: ActionFunction = ({ request }) => logout(request);
-
-export const loader: LoaderFunction = () => redirect('/');
