a222867ac75eec40077d2ea2d2c257ae594ac2b3
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2022-06-13 09:13:57 -0700

    feat: initial styling for homepage

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,14 +1,44 @@
+import { Link, useLoaderData } from '@remix-run/react';
 import type { LoaderFunction } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
 
-type LoaderData = { id: string; title: string; summary: string }[];
+type LoaderData = {
+  link: string;
+  date: string;
+  title: string;
+  summary: string;
+}[];
 
 export const loader: LoaderFunction = () => {
   const posts = [
     {
-      id: 'see-anyones-feed',
-      title: 'See anyones feed',
-      summary: 'Easily see the world from anothers perspective',
+      link: '/elonmusk/feed',
+      date: '6/10/2022',
+      title: 'see anyone’s feed',
+      summary: 'easily dive into twitter from another’s perspective',
+    },
+    {
+      link: '/changelog',
+      date: '4/20/2022',
+      title: 'follow the tweetscape in realtime',
+      summary: 'and reddit style time filters and even more goodies',
+    },
+    {
+      link: '/changelog',
+      date: '4/11/2022',
+      title: 'performance upgrades',
+      summary: 'superhuman has a 100ms rule; we have a 50ms rule',
+    },
+    {
+      link: '/changelog',
+      date: '4/3/2022',
+      title: 'we just rekt everything',
+      summary: 'see top articles from experts for any twitter list',
+    },
+    {
+      link: '/changelog',
+      date: '3/25/2022',
+      title: 'open source twitter',
+      summary: 'a few reasons why we’re building tweetscape',
     },
   ];
   return posts;
@@ -17,25 +47,42 @@ export const loader: LoaderFunction = () => {
 export default function Index() {
   const posts = useLoaderData<LoaderData>();
   return (
-    <main className='flex items-center justify-center'>
+    <main className='flex h-screen items-center justify-center'>
       <section>
         <div>
-          <h1>
-            tweetscape <i>experiments</i>
+          <h1 className='text-4xl font-medium'>
+            tweetscape <i className='text-blue-500'>experiments</i>
           </h1>
-          <p>explorations on better social media exploration</p>
+          <p className='italic text-gray-500'>
+            explorations on better social media exploration
+          </p>
         </div>
       </section>
-      <section>
-        <ul>
-          {posts.map((post) => (
-            <li key={post.id} className='bl relative pl-4'>
-              <h2>{post.title}</h2>
-              <p>{post.summary}</p>
-            </li>
-          ))}
-        </ul>
-      </section>
+      <nav className='relative ml-14'>
+        {posts.map((post) => (
+          <Link
+            key={post.title}
+            to={post.link}
+            className='group relative block border-l-2 border-gray-200 pl-6 pb-4'
+          >
+            <div className='absolute top-0 -left-1 -ml-px h-2 w-2 rounded-full bg-gray-200' />
+            <h2 className='text-xl'>
+              <span className='font-medium group-hover:underline'>
+                {post.title}
+              </span>
+              <span className='font-normal italic text-gray-500'>
+                {' '}
+                ·{' '}
+                {new Date(post.date).toLocaleString(undefined, {
+                  dateStyle: 'short',
+                })}
+              </span>
+            </h2>
+            <p className='italic text-gray-500'>{post.summary}</p>
+          </Link>
+        ))}
+        <div className='absolute bottom-0 -left-1 ml-px h-2 w-2 rounded-full bg-gray-200' />
+      </nav>
     </main>
   );
 }
