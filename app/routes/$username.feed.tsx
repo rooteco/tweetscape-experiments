b1f3a110106235e50d954edf17564ec99bb5ecd4
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2022-06-13 02:18:29 -0700

    fix: do reverse chronological sort client-side
    To ensure everything the user sees makes sense, I do one last reverse
    chronological sort client-side while rendering. This isn't necessary if
    you only make a single request to the recent search API, but, typically,
    I'm making multiple and thus once joined I have to re-sort the returned
    aggregated tweets by date.

--- app/routes/$username.feed.tsx
+++ app/routes/$username.feed.tsx
@@ -32,52 +32,58 @@ export default function Feed() {
         </button>
       </form>
       <main className='my-8 mx-auto max-w-screen-sm'>
-        {tweets.map((tweet) => (
-          <div className='mx-2 my-6 flex' key={tweet.id}>
-            <img
-              className='h-12 w-12 rounded-full border border-gray-300 bg-gray-100'
-              alt=''
-              src={tweet.author.profile_image_url}
-            />
-            <article key={tweet.id} className='ml-2.5 flex-1'>
-              <header>
-                <h3>
-                  <a
-                    href={`https://twitter.com/${tweet.author.username}`}
-                    target='_blank'
-                    rel='noopener noreferrer'
-                    className='mr-1 font-medium hover:underline'
-                  >
-                    {tweet.author.name}
-                  </a>
-                  <a
-                    href={`https://twitter.com/${tweet.author.username}`}
-                    target='_blank'
-                    rel='noopener noreferrer'
-                    className='text-sm text-gray-500'
-                  >
-                    @{tweet.author.username}
-                  </a>
-                  <span className='mx-1 text-sm text-gray-500'>·</span>
-                  <a
-                    href={`https://twitter.com/${tweet.author.username}/status/${tweet.id}`}
-                    target='_blank'
-                    rel='noopener noreferrer'
-                    className='text-sm text-gray-500 hover:underline'
-                  >
-                    <TimeAgo
-                      locale='en_short'
-                      datetime={new Date(tweet.created_at ?? new Date())}
-                    />
-                  </a>
-                </h3>
-              </header>
-              <p
-                dangerouslySetInnerHTML={{ __html: tweet.html ?? tweet.text }}
+        {tweets
+          .sort(
+            (a, b) =>
+              new Date(b.created_at as string).valueOf() -
+              new Date(a.created_at as string).valueOf()
+          )
+          .map((tweet) => (
+            <div className='mx-2 my-6 flex' key={tweet.id}>
+              <img
+                className='h-12 w-12 rounded-full border border-gray-300 bg-gray-100'
+                alt=''
+                src={tweet.author.profile_image_url}
               />
-            </article>
-          </div>
-        ))}
+              <article key={tweet.id} className='ml-2.5 flex-1'>
+                <header>
+                  <h3>
+                    <a
+                      href={`https://twitter.com/${tweet.author.username}`}
+                      target='_blank'
+                      rel='noopener noreferrer'
+                      className='mr-1 font-medium hover:underline'
+                    >
+                      {tweet.author.name}
+                    </a>
+                    <a
+                      href={`https://twitter.com/${tweet.author.username}`}
+                      target='_blank'
+                      rel='noopener noreferrer'
+                      className='text-sm text-gray-500'
+                    >
+                      @{tweet.author.username}
+                    </a>
+                    <span className='mx-1 text-sm text-gray-500'>·</span>
+                    <a
+                      href={`https://twitter.com/${tweet.author.username}/status/${tweet.id}`}
+                      target='_blank'
+                      rel='noopener noreferrer'
+                      className='text-sm text-gray-500 hover:underline'
+                    >
+                      <TimeAgo
+                        locale='en_short'
+                        datetime={new Date(tweet.created_at ?? new Date())}
+                      />
+                    </a>
+                  </h3>
+                </header>
+                <p
+                  dangerouslySetInnerHTML={{ __html: tweet.html ?? tweet.text }}
+                />
+              </article>
+            </div>
+          ))}
       </main>
     </>
   );
