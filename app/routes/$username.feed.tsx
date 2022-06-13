9f2649b889c0b1f31a8bf7e20cbf099957ffe1c2
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2022-06-13 01:13:17 -0700

    feat: use `<form>` to enable browser behavior
    This commit replaces the `<header>` wrapper element with a `<form>` to:
    a) Enable "<enter>" submissions;
    b) Show a loading state (handled by the browser) when transitioning.
    
    Eventually, I may want to replace the controlled `<input>` and the
    dynamic `action=""` property to simply be submitted in the form itself
    (as a query parameter as per convention) to support no-JS.

--- app/routes/$username.feed.tsx
+++ app/routes/$username.feed.tsx
@@ -1,4 +1,4 @@
-import { Link, useLoaderData } from '@remix-run/react';
+import { useLoaderData } from '@remix-run/react';
 import { useState } from 'react';
 
 import type { LoaderData } from '~/feed.server';
@@ -10,7 +10,11 @@ export default function Feed() {
   const [handle, setHandle] = useState('elonmusk');
   return (
     <>
-      <header className='sticky top-2 my-8 mx-auto flex max-w-sm'>
+      <form
+        method='get'
+        className='sticky top-2 my-8 mx-auto flex max-w-sm'
+        action={`/${encodeURIComponent(handle)}/feed`}
+      >
         <input
           type='text'
           placeholder='Enter any Twitter handle'
@@ -18,13 +22,13 @@ export default function Feed() {
           onChange={(evt) => setHandle(evt.currentTarget.value)}
           className='flex-1 rounded border-2 border-black px-2 py-1'
         />
-        <Link
-          to={`/${encodeURIComponent(handle)}/feed`}
+        <button
+          type='submit'
           className='ml-2 inline-block rounded border-2 border-black bg-black px-2 py-1 text-white'
         >
           See their feed
-        </Link>
-      </header>
+        </button>
+      </form>
       <ul className='my-8 mx-auto max-w-screen-sm'>
         {tweets.map((tweet) => (
           <li key={tweet.id} className='mx-2 my-6'>
