bb22363c07adb1c2d5bcf2db07e75e6ea58e7701
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2022-06-13 09:22:39 -0700

    feat: remove remix blues template app

--- app/routes/healthcheck.tsx
+++ app/routes/healthcheck.tsx
@@ -1,18 +1,15 @@
 // learn more: https://fly.io/docs/reference/configuration/#services-http_checks
 import type { LoaderFunction } from '@remix-run/node';
 
-import { prisma } from '~/db.server';
-
 export const loader: LoaderFunction = async ({ request }) => {
   const host =
     request.headers.get('X-Forwarded-Host') ?? request.headers.get('host');
-
   try {
     const url = new URL('/', `http://${host}`);
     // if we can connect to the database and make a simple query
     // and make a HEAD request to ourselves, then we're good.
+    // TODO: check redis connection here.
     await Promise.all([
-      prisma.user.count(),
       fetch(url.toString(), { method: 'HEAD' }).then((r) => {
         if (!r.ok) throw new Error(`${r.toString()} Not OK`);
         return r;
@@ -20,7 +17,6 @@ export const loader: LoaderFunction = async ({ request }) => {
     ]);
     return new Response('OK');
   } catch (error: unknown) {
-    console.log('healthcheck ❌', { error });
     return new Response('ERROR', { status: 500 });
   }
 };
