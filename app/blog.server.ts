4123ac02428b8697885e063d781812bc8cc9b416
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2022-06-13 22:57:51 -0700

    feat: setup and add mdx-powered blog posts
    This commit both implements the scaffolding required for an MDX-powered
    blog (i.e. a `loader` that compiles posts at runtime using
    `mdx-bundler`) and copies over all the existing changelog posts from the
    `tweetscape` project repository.

---
+++ app/blog.server.ts
@@ -0,0 +1,32 @@
+import fs from 'fs/promises';
+import { resolve } from 'path';
+
+import type { LoaderFunction } from '@remix-run/node';
+import { bundleMDX } from 'mdx-bundler';
+import { json } from '@remix-run/node';
+
+import { log } from '~/log.server';
+
+const BLOG_DIRECTORY = '../blog';
+
+type Meta = { date: string; author: string; title: string; summary: string };
+export type LoaderData = { frontmatter: Meta; code: string; path: string }[];
+
+export const loader: LoaderFunction = async () => {
+  const cwd = resolve(__dirname, BLOG_DIRECTORY);
+  const filenames = await fs.readdir(cwd);
+  const files = filenames.map((f) => resolve(cwd, f));
+  log.info(`Parsing markdown for ${files.length} files...`);
+  const posts = await Promise.all(
+    files.map((file) => bundleMDX<Meta>({ file, cwd }))
+  );
+  return json<LoaderData>(
+    posts
+      .map((p, idx) => ({ ...p, path: filenames[idx] }))
+      .sort(
+        (a, b) =>
+          new Date(b.frontmatter.date).valueOf() -
+          new Date(a.frontmatter.date).valueOf()
+      )
+  );
+};
