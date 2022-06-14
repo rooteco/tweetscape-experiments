07317e830e215f9a73da3471f9799dd970315fc9
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2022-06-14 00:04:57 -0700

    style: follow prettier tailwind classes order

--- app/components/sparkles.tsx
+++ app/components/sparkles.tsx
@@ -29,9 +29,9 @@ function Sparkle({ size, color, style }: SparkleProps) {
   const path =
     'M26.5 25.5C19.0043 33.3697 0 34 0 34C0 34 19.1013 35.3684 26.5 43.5C33.234 50.901 34 68 34 68C34 68 36.9884 50.7065 44.5 43.5C51.6431 36.647 68 34 68 34C68 34 51.6947 32.0939 44.5 25.5C36.5605 18.2235 34 0 34 0C34 0 33.6591 17.9837 26.5 25.5Z';
   return (
-    <span className='animate-sparkle-ping absolute block' style={style}>
+    <span className='absolute block animate-sparkle-ping' style={style}>
       <svg
-        className='animate-sparkle-spin block'
+        className='block animate-sparkle-spin'
         width={size}
         height={size}
         viewBox='0 0 68 68'
